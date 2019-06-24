fcf0d91243bcf0201b065f596eadbe306a3b250a
César <cesar.mcid@gmail.com>
2019-06-23 22:54:07 -0400

    Commit Inicial :)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Pizzicato from 'pizzicato';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,34 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public btn_text:String;
+  public btn_icon:String;
+  public image:String;
+  public static sound:Boolean
+  protected amar_azul:Pizzicato;
+  constructor() {
+    HomePage.sound = false
+    this.btn_text = 'Iniciar el llamado a beberrr';
+    this.btn_icon = 'musical-note';
+    this.image = 'perrito_triste.gif';
+  }
 
+  ngOnInit(){
+    this.amar_azul = new Pizzicato.Sound('/assets/amar_azul.mp3');
+  }
+
+  sound(){
+    HomePage.sound = !HomePage.sound
+    if(HomePage.sound){
+      this.amar_azul.play();
+      this.btn_text = 'Detener el llamado a beberrr';
+      this.btn_icon = 'volume-off';
+      this.image = 'cervecita.gif';
+    }else{
+      this.btn_text = 'Iniciar el llamado a beberrr';
+      this.amar_azul.stop();
+      this.btn_icon = 'musical-note';
+      this.image = 'perrito_triste.gif';
+    }
+  }
 }
